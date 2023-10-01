db596084f512a3567d774372485bc88c185c1bb0
Pablo-notebook <juarezjpablo7@gmail.com>
2023-10-01 12:58:48 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Numeracion.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Entidades
+{
+    public class Numeracion
+    {
+        public enum Esistema { Decimal, Binario };
+        private Esistema sistema;
+        private double valorNumerico;
+        
+        
+        public string Valor
+            {
+            get
+                {
+                    return this.sistema;
+                 }
+            
+            }
+        public Numeracion(Esistema sistema,double valorNum)
+        {
+            this.sistema=sistema;
+            this.valorNumerico=valorNum;
+        }
+
+
+
+    }
+}
---
+++ MiCalculadora/Form1.cs
@@ -0,0 +1,10 @@
+namespace MiCalculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

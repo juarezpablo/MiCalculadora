ec3c59f564e00e20569d3492def29aec4d1c268b
Pablo-notebook <juarezjpablo7@gmail.com>
2023-10-05 16:16:26 -0300

    funcionando

--- Entidades/Numeracion.cs
+++ Entidades/Numeracion.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
+
+
 namespace Entidades
 {
     public class Numeracion
@@ -13,17 +15,167 @@ namespace Entidades
             {
             get
                 {
-                    return this.sistema;
+                    return this.valorNumerico.ToString();
                  }
             
             }
+
+        public Esistema Sistema
+        {
+            get { return this.sistema; }
+        }
+
         public Numeracion(Esistema sistema,double valorNum)
+            
         {
             this.sistema=sistema;
             this.valorNumerico=valorNum;
         }
+        public Numeracion(Esistema sistema, string valor)
+        {
+            InicializarValores(sistema, valor);
+        }
 
 
+        private void InicializarValores(Esistema sistema,string valor)
+        {
+            if (valor != "")
+            {
+                if (sistema == Esistema.Binario && EsBinario(valor))
+                {
+                    this.valorNumerico = BinarioADecimal(valor);
+                }
+                else
+                {
+                    sistema = Esistema.Decimal;
+                    this.valorNumerico = int.Parse(valor);
+                }
+            }
+            else { 
+                sistema= Esistema.Decimal;
+                this.valorNumerico = double.MinValue;
+            }
+        }
 
+        private bool EsBinario(string valor)
+        {
+            bool retorno=false;
+            foreach(char numero in valor)
+            {
+                if(numero.Equals(0) || numero.Equals(1))
+                {
+                    retorno=true;
+                }
+                else
+                {
+                    retorno=false;
+                }
+            }
+            return retorno;
+        }
+
+        public string ConvertirA(Esistema sistema)
+        {
+           
+            if (sistema == Esistema.Binario)
+            {
+                return DecimalABinario(Valor);
+            }
+            else
+            {
+                return BinarioADecimal(Valor).ToString();
+            }
+        }
+
+        private double BinarioADecimal(string valor)
+        {
+            if (EsBinario(valor))
+            {
+                //conversion binario a decimal y retorno el resultado tipo double
+                
+                double largo = valor.Length;
+                double aux =0;
+                double acumulador = 0;
+                foreach(char numero in valor)
+                {
+                    if (numero.Equals(1))
+                    {
+                        acumulador = acumulador + Math.Pow(2, (largo - aux));
+
+                    }
+                    aux++;
+                }
+                return acumulador;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private string DecimalABinario(string valor)
+        {
+            //convertir de decimal a binario y retornarlo como string
+            string numeroBinario=string.Empty;
+            int numero=int.Parse(valor);
+            int resto;
+            int resultadoDiv=numero;
+            do
+            {
+                resto = resultadoDiv % 2;
+                resultadoDiv = resultadoDiv / 2;
+                numeroBinario=resto.ToString()+numeroBinario;
+
+            } while (resultadoDiv > 0);
+
+            return numeroBinario ;
+        }
+        private string DecimalABinario(int valor)
+        {
+            return DecimalABinario(valor.ToString());
+        }
+
+        public static Numeracion operator +(Numeracion n1, Numeracion n2)
+        {
+             double resultado =n1.valorNumerico + n2.valorNumerico;
+             Numeracion nuevoNum = new Numeracion(Esistema.Decimal,resultado);
+             return nuevoNum;
+        }
+
+        public static Numeracion operator -(Numeracion n1, Numeracion n2)
+        {
+            double resultado = n1.valorNumerico - n2.valorNumerico;
+            Numeracion nuevoNum = new Numeracion(Esistema.Decimal, resultado);
+            return nuevoNum;
+        }
+
+        public static Numeracion operator /(Numeracion n1, Numeracion n2)
+        {
+            double resultado = n1.valorNumerico / n2.valorNumerico;
+            Numeracion nuevoNum = new Numeracion(Esistema.Decimal, resultado);
+            return nuevoNum;
+        }
+        public static Numeracion operator *(Numeracion n1, Numeracion n2)
+        {
+            double resultado = n1.valorNumerico * n2.valorNumerico;
+            Numeracion nuevoNum = new Numeracion(Esistema.Decimal, resultado);
+            return nuevoNum;
+        }
+      //  public static bool operator ==(Numeracion n1, Numeracion n2)
+      //  {
+      //      return n1.sistema == n2.sistema;
+      //  }
+      //  public static bool operator !=(Numeracion n1, Numeracion n2)
+      //  {
+      //      return !(n1== n2);
+      //  }
+      //  public static bool operator ==( Esistema sistema, Numeracion n1)
+      //  {
+      //      return n1.Sistema==sistema;
+      //  }
+      //  public static bool operator!=(Esistema sistema, Numeracion n1)
+      //  {
+      //      return !(n1.Sistema == sistema);
+      //  }
     }
 }
---
+++ MiCalculadora/FrmCalculadora.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace MiCalculadora
+{
+    partial class FrmCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            rdbBinario = new RadioButton();
+            rdbDecimal = new RadioButton();
+            label1 = new Label();
+            lblResultado = new Label();
+            lblPrimerOperador = new Label();
+            lblOperacion = new Label();
+            lblSegundoOperador = new Label();
+            txtPrimerOperador = new TextBox();
+            txtSegundoOperador = new TextBox();
+            cmbOperacion = new ComboBox();
+            btnOperar = new Button();
+            btnCerrar = new Button();
+            btnLimpiar = new Button();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rdbBinario);
+            groupBox1.Controls.Add(rdbDecimal);
+            groupBox1.Location = new Point(186, 104);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(311, 94);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Representar resultado en:";
+            // 
+            // rdbBinario
+            // 
+            rdbBinario.AutoSize = true;
+            rdbBinario.Location = new Point(178, 41);
+            rdbBinario.Margin = new Padding(3, 2, 3, 2);
+            rdbBinario.Name = "rdbBinario";
+            rdbBinario.Size = new Size(62, 19);
+            rdbBinario.TabIndex = 1;
+            rdbBinario.TabStop = true;
+            rdbBinario.Text = "Binario";
+            rdbBinario.UseVisualStyleBackColor = true;
+            rdbBinario.CheckedChanged += rdbBinario_CheckedChanged;
+            // 
+            // rdbDecimal
+            // 
+            rdbDecimal.AutoSize = true;
+            rdbDecimal.Location = new Point(50, 41);
+            rdbDecimal.Margin = new Padding(3, 2, 3, 2);
+            rdbDecimal.Name = "rdbDecimal";
+            rdbDecimal.Size = new Size(68, 19);
+            rdbDecimal.TabIndex = 0;
+            rdbDecimal.TabStop = true;
+            rdbDecimal.Text = "Decimal";
+            rdbDecimal.UseVisualStyleBackColor = true;
+            rdbDecimal.CheckedChanged += rdbDecimal_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(90, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Resultado:";
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblResultado.Location = new Point(324, 58);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 21);
+            lblResultado.TabIndex = 2;
+            // 
+            // lblPrimerOperador
+            // 
+            lblPrimerOperador.AutoSize = true;
+            lblPrimerOperador.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPrimerOperador.Location = new Point(90, 210);
+            lblPrimerOperador.Name = "lblPrimerOperador";
+            lblPrimerOperador.Size = new Size(142, 21);
+            lblPrimerOperador.TabIndex = 3;
+            lblPrimerOperador.Text = "Primer Operador:";
+            // 
+            // lblOperacion
+            // 
+            lblOperacion.AutoSize = true;
+            lblOperacion.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblOperacion.Location = new Point(304, 210);
+            lblOperacion.Name = "lblOperacion";
+            lblOperacion.Size = new Size(93, 21);
+            lblOperacion.TabIndex = 4;
+            lblOperacion.Text = "Operacion:";
+            // 
+            // lblSegundoOperador
+            // 
+            lblSegundoOperador.AutoSize = true;
+            lblSegundoOperador.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSegundoOperador.Location = new Point(439, 210);
+            lblSegundoOperador.Name = "lblSegundoOperador";
+            lblSegundoOperador.Size = new Size(159, 21);
+            lblSegundoOperador.TabIndex = 5;
+            lblSegundoOperador.Text = "Segundo Operador:";
+            // 
+            // txtPrimerOperador
+            // 
+            txtPrimerOperador.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPrimerOperador.Location = new Point(90, 239);
+            txtPrimerOperador.Margin = new Padding(3, 2, 3, 2);
+            txtPrimerOperador.Name = "txtPrimerOperador";
+            txtPrimerOperador.Size = new Size(168, 29);
+            txtPrimerOperador.TabIndex = 6;
+            txtPrimerOperador.TextChanged += txtPrimerOperador_TextChanged;
+            // 
+            // txtSegundoOperador
+            // 
+            txtSegundoOperador.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSegundoOperador.Location = new Point(439, 239);
+            txtSegundoOperador.Margin = new Padding(3, 2, 3, 2);
+            txtSegundoOperador.Name = "txtSegundoOperador";
+            txtSegundoOperador.Size = new Size(171, 29);
+            txtSegundoOperador.TabIndex = 7;
+            txtSegundoOperador.TextChanged += txtSegundoOperador_TextChanged;
+            // 
+            // cmbOperacion
+            // 
+            cmbOperacion.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacion.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            cmbOperacion.FormattingEnabled = true;
+            cmbOperacion.Location = new Point(284, 238);
+            cmbOperacion.Margin = new Padding(3, 2, 3, 2);
+            cmbOperacion.Name = "cmbOperacion";
+            cmbOperacion.Size = new Size(133, 29);
+            cmbOperacion.TabIndex = 8;
+            // 
+            // btnOperar
+            // 
+            btnOperar.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOperar.Location = new Point(90, 281);
+            btnOperar.Margin = new Padding(3, 2, 3, 2);
+            btnOperar.Name = "btnOperar";
+            btnOperar.Size = new Size(168, 35);
+            btnOperar.TabIndex = 9;
+            btnOperar.Text = "Operar";
+            btnOperar.UseVisualStyleBackColor = true;
+            btnOperar.Click += btnOperar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCerrar.Location = new Point(442, 281);
+            btnCerrar.Margin = new Padding(3, 2, 3, 2);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(168, 35);
+            btnCerrar.TabIndex = 10;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.FlatAppearance.BorderSize = 3;
+            btnLimpiar.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLimpiar.Location = new Point(265, 281);
+            btnLimpiar.Margin = new Padding(3, 2, 3, 2);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(171, 35);
+            btnLimpiar.TabIndex = 11;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(716, 132);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(170, 184);
+            listBox1.TabIndex = 12;
+            // 
+            // FrmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(969, 469);
+            Controls.Add(listBox1);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnOperar);
+            Controls.Add(cmbOperacion);
+            Controls.Add(txtSegundoOperador);
+            Controls.Add(txtPrimerOperador);
+            Controls.Add(lblSegundoOperador);
+            Controls.Add(lblOperacion);
+            Controls.Add(lblPrimerOperador);
+            Controls.Add(lblResultado);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "FrmCalculadora";
+            Text = "Calculadora Juarez Pablo";
+            FormClosing += FrmCalculadora_FormClosing;
+            Load += FrmCalculadora_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton rdbBinario;
+        private RadioButton rdbDecimal;
+        private Label label1;
+        private Label lblResultado;
+        private Label lblPrimerOperador;
+        private Label lblOperacion;
+        private Label lblSegundoOperador;
+        private TextBox txtPrimerOperador;
+        private TextBox txtSegundoOperador;
+        private ComboBox cmbOperacion;
+        private Button btnOperar;
+        private Button btnCerrar;
+        private Button btnLimpiar;
+        private ListBox listBox1;
+    }
+}
